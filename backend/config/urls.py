d4373b6b07afb155d928e8db793aee05df985658
Artashes Soghomonyan <artashessoghomonyan@gmail.com>
2026-07-12 15:43:02 +0400

    Initial commit

---
+++ backend/config/urls.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
+
+class APIHealthView(APIView):
+    def get(self, request):
+        return Response({
+            "message": "OK",
+        }, status=status.HTTP_200_OK)
+
+urlpatterns = [
+    path("api/health/", APIHealthView.as_view(), name="health"),
+]
+
+if settings.DEBUG:
+    urlpatterns.append(path("admin/", admin.site.urls))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns.append(path(f"{settings.ADMIN_PATH}/", admin.site.urls))
